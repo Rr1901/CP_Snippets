28df2c762614075441610fcda886f9f041d5c43c
Rahul sannigrahi <60656712+Rr1901@users.noreply.github.com>
2021-10-01 08:26:48 +0530

    Update PrimeCheck.cpp

--- C++/PrimeCheck.cpp
+++ C++/PrimeCheck.cpp
@@ -10,15 +10,19 @@ DATE: 1/10/2021
 using namespace std;
 
 //SNIPPET
-bool isPrime(int n)
-{
-    for (int i = 2; i <= int(sqrt(n) + 1); i++)
-    {
-        if (n % i == 0)
-            return false;
-    }
-    return true;
-}
+bool isPrime(int n) 
+{ 
+    if (n <= 1)  return false; 
+    if (n <= 3)  return true; 
+  
+    if (n%2 == 0 || n%3 == 0) return false; 
+  
+    for (int i=5; i*i<=n; i=i+6) //6n+1 and 6n-1
+        if ((n%i==0)|| n%(i+2)==0) 
+           return false; 
+  
+    return true; 
+} 
 int main()
 {
     //APPLICATION
